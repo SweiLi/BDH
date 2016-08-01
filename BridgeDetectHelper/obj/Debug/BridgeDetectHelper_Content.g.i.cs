e1149c52c2861f6e18359a7f67d3966b416a2a7a
SweiLi <448808451@qq.com>
2016-08-01 18:34:36 +0800

    提交

--- BridgeDetectHelper/obj/Debug/BridgeDetectHelper_Content.g.i.cs
+++ BridgeDetectHelper/obj/Debug/BridgeDetectHelper_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/bridge.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons/pic_joint.png")]
 
 
